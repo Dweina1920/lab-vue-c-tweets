d11d617a3f1b6d547e8dfe82165a3321421afc55
Dina <dina.yamanid@gmail.com>
2023-02-20 13:37:24 +0100

    finished

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,36 +1,19 @@
 <template>
   <div class="tweet">
-    <img
-      src="https://i.imgur.com/9yw1Fyw.jpg"
-      class="profile"
-      alt="profile"
-    />
+      <ProfileImage :src="props.tweet.user.image"/>
 
-    <div class="body">
-      <div class="top">
-        <span class="user">
-          <span class="name">Ironhack</span>
-          <span class="handle">@ironhack</span>
-        </span>
+      <div class="body">
+          <div class="top">
+             <User :name="props.tweet.user.name" :handle="props.tweet.user.handle" />
+              <Timestamp :timestamp="props.tweet.timestamp" />
+          </div>
 
-        <span class="timestamp">Nov 30, 2020</span>
+         <Message :message="props.tweet.message" />
+
+          <Actions />
       </div>
 
-      <p class="message">
-        On December 7th, we will be hosting a #webinar that will introduce you
-        to #SQL! Are you ready? 🚀
-      </p>
-
-      <div class="actions">
-        <!-- Font Awesome icons -->
-        <i class="far fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-        <i class="fas fa-share"></i>
-      </div>
-    </div>
-
-    <i class="fas fa-ellipsis-h"></i>
+      <i class="fas fa-ellipsis-h"></i>
   </div>
 </template>
 
@@ -39,3 +22,12 @@ a {
   color: #42b983;
 }
 </style>
+<script setup>
+import { defineProps } from "vue";
+import Timestamp from "./Timestamp.vue";
+import User from "./User.vue";
+import ProfileImage from "./ProfileImage.vue";
+import Message from "./Message.vue";
+import Actions from "./Actions.vue";
+const props = defineProps(["tweet"]);
+</script>
